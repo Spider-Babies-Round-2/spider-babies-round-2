a15faeb93aabd63f45a330e119791f220d8a06c2
nananananate <piewalker442@gmail.com>
2020-12-08 10:58:05 -0800

    can now specify class types in InfoGains

--- utils/infogains.py
+++ utils/infogains.py
@@ -1,9 +1,16 @@
 import numpy as np
 
 class InfoGains():
-    def __init__(self, data: np.array):
+    def __init__(self, data: np.array, class_types: tuple = (0,1):
+        '''
+        Takes in data of features + classes
+        also takes a tuple of class_types (must be binary)
+        '''
         self.features = data[:,:-1]
         self.classes = data[:,-1]
+                 
+        assert len(class_types) == 2
+        self.class_types = class_types
 
         self.entropy = self.calc_entropy(self.classes)
         self.p_class_a = np.count_nonzero(self.classes == 1) / len(self.classes)
@@ -39,8 +46,8 @@ class InfoGains():
 
     def calc_info_gain_of_feature(self, feature: np.array) -> float:
         #print("--- info gain of feature ---")
-        entropy_left = self.calc_entropy_of_feature(feature,0)
-        entropy_right = self.calc_entropy_of_feature(feature,1)
+        entropy_left = self.calc_entropy_of_feature(feature,self.class_types[0])
+        entropy_right = self.calc_entropy_of_feature(feature,self.class_types[1])
 
 
         weighted_entropy = entropy_left*(1 - feature.mean()) + entropy_right*(feature.mean())
